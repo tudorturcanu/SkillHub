580139ca5be4caa1dc00cbf97530ed1c3981ec70
Tudor Turcanu <tudorturcanu@gmail.com>
2026-06-05 21:36:23 +0200

    Initial Commit

---
+++ SkillKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SkillKit
+//
+//  Created by tudor on 05.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SkillKit/SkillKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillKitApp.swift
+//  SkillKit
+//
+//  Created by tudor on 05.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct SkillKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
